bee226db9431d7a4bafc5d894d049461029ec9ae
Jesus Hilario Hernandez <jesushilariohernandez@users.noreply.github.com>
2017-01-15 13:54:52 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var btn = document.getElementById('btn');
 function myFunction(){
     var x = document.getElementById("myText").value;
     var ourRequest = new XMLHttpRequest();
-    ourRequest.open('GET', 'http://api.wordnik.com:80/v4/word.json/' + x + '/definitions?limit=2&includeRelated=true&useCanonical=false&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5');
+    ourRequest.open('GET', 'http://api.wordnik.com:80/v4/word.json/' + x + '/definitions?limit=2&includeRelated=true&useCanonical=false&includeTags=false&api_key=551cd772a6bd0f92b40010e295e0739d0acaf17d08ecc3c9d');
     ourRequest.onload = function(){
         var ourData = JSON.parse(ourRequest.responseText);
         renderHTML(ourData);
