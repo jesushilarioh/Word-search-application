02ba44b60e747aa2a9c42139b43bc16cf104745d
jesushilariohernandez <jesushilariohernandez@gmail.com>
2017-02-09 05:02:48 -0600

    modified index.js file

--- index.js
+++ index.js
@@ -1,36 +1,39 @@
 // Variables
 var animalContainer = document.getElementById('demo');
 var btn = document.getElementById('btn');
+var x = document.getElementById("myText");
 
-// Main function
-function myFunction(){
-    var x = document.getElementById("myText");
-    var ourRequest = new XMLHttpRequest();
-    ourRequest.open('GET', 'http://api.wordnik.com:80/v4/word.json/' + x.value + '/definitions?limit=2&includeRelated=true&useCanonical=false&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5');
-    ourRequest.onload = function(){
-        var ourData = JSON.parse(ourRequest.responseText);
-        renderHTML(ourData);
 
-        x.addEventListener("keypress", function() {
-          console.log(ourData[0]);
-          var htmlString = "";
+x.addEventListener('keyup', function() {
+  var ourRequest = new XMLHttpRequest();
 
-          for (i = 0; i < ourData.length; i++){
-              animalContainer.innerHTML = htmlString += ourData[0];
-          }
-        });
+  ourRequest.open('GET', 'http://api.wordnik.com:80/v4/word.json/' + x.value + '/definitions?limit=2&includeRelated=true&useCanonical=false&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5');
+  ourRequest.onload = function(){
+      var ourData = JSON.parse(ourRequest.responseText);
+      renderHTML(ourData);
 
-    };
+        console.log('hello');
+        if (x.value === "") {
+          ourData = ourRequest.responseText;
+        }
 
-    ourRequest.send();
-}
+  };
+
+  ourRequest.send();
+});
+
+
+
 
 // Secondary functions
 function renderHTML(data) {
     var htmlString = "";
+if (data){
+  for (i = 0; i < data.length; i++){
+      animalContainer.innerHTML = htmlString += '<h3>' + data[i].text + '</h3><p>TAKEN FROM: ' + data[i].attributionText + '.</p>';
+  }
+} else {
+  htmlString += "";
+}
 
-    for (i = 0; i < data.length; i++){
-        htmlString += '<h3>' + data[i].text + '</h3><p>TAKEN FROM: ' + data[i].attributionText + '.</p>';
-    }
-    animalContainer.insertAdjacentHTML('beforeend', htmlString );
 }
